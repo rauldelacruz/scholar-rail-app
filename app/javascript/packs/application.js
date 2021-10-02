30bc313e7968fe5b13bc5604e8813e99617f4d32
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-02 12:59:25 -0500

    Bootsrap 5 and fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,8 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import * as bootstrap from 'bootstrap'
+import "../stylesheets/application"
+
+import "@fortawesome/fontawesome-free/css/all"
