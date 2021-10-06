fb5ab9556cf2e0b2dbbaa283ed62390db0c025a2
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-05 21:49:35 -0500

    devise fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
 
+  root "static_pages#landing_page"
   # get 'static_pages/landing_page'
   # get 'static_pages/privacy_policy'
   get "privacy_policy", to: "static_pages#privacy_policy"
