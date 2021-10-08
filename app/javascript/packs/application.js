8f99b1dcb19e043615429d7427477fed898cdb65
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-08 17:20:05 -0500

    styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,7 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'stylesheets/application'
 import "@fortawesome/fontawesome-free/css/all"
+
+global.toastr = require("toastr")
+
+import "../stylesheets/application"
