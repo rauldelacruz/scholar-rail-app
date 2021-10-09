3b9d8b19f1313e2fece3f4d9adbb49a29c46aeab
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-08 23:30:02 -0500

    devise invitable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :require_admin, only: [:edit, :update, :ban, :destroy]
+  before_action :require_admin, only: [:edit, :update, :ban, :destroy, :resend_confirmation_instructions]
+  before_action :require_admin_or_inviter, only: [:resend_invitation]
 
     def index
       @users = User.all.order(created_at: :desc)
@@ -40,6 +41,26 @@ class UsersController < ApplicationController
       redirect_to @user, notice: "User locked: #{@user.access_locked?}"
     end
 
+    def resend_confirmation_instructions
+      @user = User.find(params[:id])
+      if @user.confirmed? == false && @user.created_by_invite? == false
+        @user.resend_confirmation_instructions
+        redirect_to @user, notice: "Confirmation instructions were resent"
+      else
+        redirect_to @user, alert: "User already confirmed"
+      end
+    end
+
+    def resend_invitation
+      @user = User.find(params[:id])
+      if @user.created_by_invite? && @user.invitation_accepted? == false && @user.confirmed? == false
+        @user.invite!
+        redirect_to @user, notice: "Invitation was resent"
+      else
+        redirect_to @user, alert: "User already confirmed"
+      end
+    end
+
     private
 
     def user_params
@@ -50,6 +71,13 @@ class UsersController < ApplicationController
       # unless current_user.admin? || current_user.teacher?
       unless current_user.admin?
         redirect_to root_path, alert: "You need authorization to perform this action"
+      end
+    end
+
+    def require_admin_or_inviter
+      @user = User.find(params[:id])
+      unless current_user.admin? || @user.invited_by == current_user
+        redirect_to root_path, alert: "You are not authorized to perform this action"
       end
     end
 
