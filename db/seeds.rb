31bca71b0ca0bba17eb569c8359ac015e952b769
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-11 11:40:46 -0500

    services

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 User.create(email: "admin@example.com", password: "123456", password_confirmation: "admin@example.com")
 User.create(email: "student@example.com", password: "123456", password_confirmation: "student@example.com")
 User.update_all confirmed_at: DateTime.now
+
+Classroom.create(name: "Room1")
+Classroom.create(name: "Room2")
+Classroom.create(name: "Room3")
+
+Service.create(name: "Algebra", duration: 45, client_price: 0)
+Service.create(name: "Javascript", duration: 45, client_price: 0)
+Service.create(name: "Italian", duration: 45, client_price: 0)
