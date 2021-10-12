deeae884ac9809e79f64c6d8f255763cf8262784
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-11 23:17:48 -0500

    lessons fixes

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,8 @@
 class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :classroom
-
+  belongs_to :course
+  
   validates :status, :start, presence: true
 
   STATUSES = [:planned, :confirmed, :cancelled]
