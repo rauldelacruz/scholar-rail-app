ec8b0ba9fae7055c0443e6faafaf2dc207d7e1e1
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-11 21:12:55 -0500

    courses schedules

---
+++ db/migrate/20211012010416_add_schedule_to_course.rb
@@ -0,0 +1,6 @@
+class AddScheduleToCourse < ActiveRecord::Migration[6.1]
+  def change
+    add_column :courses, :start_time, :time
+    add_column :courses, :days, :jsonb, null: false, default: {}
+  end
+end
