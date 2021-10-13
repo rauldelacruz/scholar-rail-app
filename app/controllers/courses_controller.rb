6df83845d7555f84cd39c273e7b649bbc845ad3a
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-12 23:15:53 -0500

    enrollment to courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -20,6 +20,7 @@ class CoursesController < ApplicationController
   # GET /courses/1 or /courses/1.json
   def show
     @lessons = @course.lessons
+    @enrollments = @course.enrollments
   end
 
   # GET /courses/new
