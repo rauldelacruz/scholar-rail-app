3d58b88f94af3870c2ac3a753873c66a7dbf391a
rauldelacruz <raul.delacruz120@gmail.com>
2021-10-18 10:07:03 -0500

    calendar filters

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,28 @@ class StaticPagesController < ApplicationController
   end
 
   def calendar
-    @lessons = Lesson.all
+    @users = User.order(email: :asc)
+    @classrooms = Classroom.order(name: :asc)
+    @courses = Course.order(id: :desc)
+    
+    if params.has_key?(:user_id)
+      lessons = Lesson.includes(:user, :classroom, :course, :attendances)
+      @user = params[:user_id]
+      @classroom = params[:classroom_id]
+      @course = params[:course_id]
+
+      if @user.present?
+        lessons = lessons.where(user_id: @user)
+      end
+      if @classroom.present?
+        lessons = lessons.where(classroom_id: @classroom)
+      end
+      if @course.present?
+        lessons = lessons.where(course_id: @course)
+      end
+      @lessons = lessons.all
+    else
+      @lessons = Lesson.includes(:user, :classroom, :course, :attendances)
+    end
   end
 end
